f6b3ec6573c5e5d44dbc9f23451825faac454b7f
jc-geekFit <cedric.hazoume@gmail.com>
2019-05-05 20:52:16 +0100

    Ajout fichier 'followedCourses.js'

---
+++ followedCourses.js
@@ -0,0 +1,5 @@
+export let courses = [
+  { name: 'Démarrez votre projet avec Python', status: 'Completed'},
+  { name: 'Découvrez la programmation orienté objet avec Python', status: 'Completed'},
+  { name: 'Découvrez le fonctionnement des algorithmes', status: 'Completed'}
+]
